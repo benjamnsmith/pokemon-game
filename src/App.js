6027324993959d502dff7d1e8b2f9f190c24cd57
Ben Smith <benjaminwrobelsmith@gmail.com>
2022-11-26 22:55:20 +0100

    beginning work on styling

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import Information from "./Information";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Information />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Information />} />
+    //     <Route path="/" element={<Information />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div className="stage">
+      <div className="side one" grid-column-span="1 2">
+        <Information />
+      </div>
+      <div className="middle" grid-column-span="2 3">
+        <p color="white">VERSUS</p>
+      </div>
+      <div className="side two" grid-column-span="3 4">
+        <Information />
+      </div>
+    </div>
   );
 };
 
